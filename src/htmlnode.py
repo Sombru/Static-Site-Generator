204393a10c98f6434e9382ff1fa94f39e6d990e5
Sombru <142216472+Sombru@users.noreply.github.com>
2026-08-09 18:32:38 +0200

    added HTMLNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,9 +1,29 @@
+from __future__ import annotations
+
+
 class HTMLNode:
     def __init__(
-        self, tag: str = None, value: str, children: list[HTMLNode], props: dict[str:str]
+        self,
+        tag: str = None,
+        value: str = None,
+        children: list[HTMLNode] = None,
+        props: dict[str, str] = None,
     ):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
-  
+
+    def to_html(self):
+        raise NotImplementedError()
+
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        res = ""
+        for key, value in self.props.items():
+            res += f' {key}="{value}"'
+        return res
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -8,10 +9,24 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD, "aba")
         node3 = TextNode("This is a text node", TextType.LINK, "https://boot.dev")
         node4 = TextNode("This is a text node", TextType.LINK, "https://boot.dev")
-        
+
         self.assertEqual(node, node2)
         self.assertNotEqual(node2, node3)
         self.assertEqual(node3, node4)
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode(
+            props={
+                "href": "https://www.google.com",
+                "target": "_blank",
+            }
+        )
+        self.assertEqual(
+            node.props_to_html(), ' href="https://www.google.com" target="_blank"'
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
