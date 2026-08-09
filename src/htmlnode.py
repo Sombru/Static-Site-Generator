02c05895815d5b85b4774069c24a16854014a0b6
Sombru <142216472+Sombru@users.noreply.github.com>
2026-08-09 18:56:57 +0200

    added leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,7 +17,7 @@ class HTMLNode:
     def to_html(self):
         raise NotImplementedError()
 
-    def props_to_html(self):
+    def props_to_html(self) -> str:
         if not self.props:
             return ""
         res = ""
@@ -27,3 +27,18 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self) -> str:
+        if not self.value:
+            raise ValueError()
+        if not self.tag:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.props})"
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -26,6 +26,19 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             node.props_to_html(), ' href="https://www.google.com" target="_blank"'
         )
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        node2 = LeafNode("p", "This is a paragraph of text.")
+        node3 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        node4 = LeafNode(None, "No TAG!!")
+
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        self.assertEqual(node2.to_html(), "<p>This is a paragraph of text.</p>")
+        self.assertEqual(node3.to_html(), '<a href="https://www.google.com">Click me!</a>')
+        self.assertEqual(node4.to_html(), "No TAG!!")
 
 
 if __name__ == "__main__":
